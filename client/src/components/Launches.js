4227b95aa8e65bd24f26674fd8fd6ec134993946
Sachit Karki <sachitjungkarki@gmail.com>
2019-08-14 10:34:11 +0545

    Display launch items

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -1,6 +1,7 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
+import LaunchItem from "./LaunchItem";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -15,7 +16,7 @@ const LAUNCHES_QUERY = gql`
 export class Launches extends Component {
   render() {
     return (
-      <div>
+      <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
@@ -23,10 +24,17 @@ export class Launches extends Component {
             if (error) console.log(error);
 
             console.log(data);
-            return <h1>test</h1>;
+
+            return (
+              <Fragment>
+                {data.launches.map(launch => (
+                  <LaunchItem key={launch.flight_number} launch={launch} />
+                ))}
+              </Fragment>
+            );
           }}
         </Query>
-      </div>
+      </Fragment>
     );
   }
 }
