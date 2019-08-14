87822c30f42693f789548d813e4222ee21a9374f
Sachit Karki <sachitjungkarki@gmail.com>
2019-08-14 08:14:50 +0545

    react setup with launches data load

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import ApolloClient from "apollo-boost";
+import { ApolloProvider } from "react-apollo";
+
+import Launches from "./components/Launches";
+
+import logo from "./logo.png";
+import "./App.css";
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="container">
+        <img
+          src={logo}
+          alt="SpaceX"
+          style={{ width: 300, display: "block", margin: "auto" }}
+        />
+        <Launches />
+      </div>
+    </ApolloProvider>
   );
 }
 
--- server.js
+++ server.js
@@ -1,14 +1,21 @@
-const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const express = require("express");
+const graphqlHTTP = require("express-graphql");
+const cors = require("cors");
 
-const schema = require('./schema');
+const schema = require("./schema");
 
 const app = express();
 
-app.use('/graphql', graphqlHTTP({
+// ALlow cross-origin
+app.use(cors());
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
     schema,
     graphiql: true
-}));
+  })
+);
 
 const PORT = process.env.PORT || 5000;
 
