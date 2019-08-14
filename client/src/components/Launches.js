f558b62f1f58311af6f2734dff0bf95f4aec9ddc
Sachit Karki <sachitjungkarki@gmail.com>
2019-08-14 13:31:58 +0545

    Mission key display

--- client/src/components/Launches.js
+++ client/src/components/Launches.js
@@ -2,6 +2,7 @@ import React, { Component, Fragment } from "react";
 import gql from "graphql-tag";
 import { Query } from "react-apollo";
 import LaunchItem from "./LaunchItem";
+import MissionKey from "./MissionKey";
 
 const LAUNCHES_QUERY = gql`
   query LaunchesQuery {
@@ -9,6 +10,7 @@ const LAUNCHES_QUERY = gql`
       flight_number
       mission_name
       launch_date_local
+      launch_success
     }
   }
 `;
@@ -18,12 +20,11 @@ export class Launches extends Component {
     return (
       <Fragment>
         <h1 className="display-4 my-3">Launches</h1>
+        <MissionKey />
         <Query query={LAUNCHES_QUERY}>
           {({ loading, error, data }) => {
             if (loading) return <h4>Loading...</h4>;
             if (error) console.log(error);
-
-            console.log(data);
 
             return (
               <Fragment>
