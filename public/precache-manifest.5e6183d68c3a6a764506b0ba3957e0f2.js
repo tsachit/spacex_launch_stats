899b355e4b475d5d0be20b69c745691f889867ac
Sachit Karki <sachitjungkarki@gmail.com>
2019-08-15 07:21:46 +0545

    Public folder generate and final changes, and deploy

---
+++ public/precache-manifest.5e6183d68c3a6a764506b0ba3957e0f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cdf870c142b7435fb8588cd05ffacb5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "898ef8f66bd49dc731d6",
+    "url": "/static/css/main.cfda8877.chunk.css"
+  },
+  {
+    "revision": "3ab2b80347307b32dfbb",
+    "url": "/static/js/2.67d382a9.chunk.js"
+  },
+  {
+    "revision": "898ef8f66bd49dc731d6",
+    "url": "/static/js/main.cc35acf4.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  }
+]);
